27ad07eb1fde5db2358da3619c2d71c05b879cbd
HoussamEzzin <ezzinhoussam@gmail.com>
2020-11-10 11:15:26 +0100

    update the js file

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var btn = document.getElementById("generate");
+//Global variables that we will need
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const btn = document.getElementById("generate");
 
+//Initializing the current background
 color1.value="#ff0000";
 color2.value="#ffff00";
 body.style.background = "linear-gradient(to right, red, yellow)";
@@ -12,7 +14,8 @@ css.textContent = body.style.background + ";";
 
 
 
-//Do Not Repeat Yourself
+//!Do Not Repeat Yourself
+//function that set the background of our choice 
 function setGradient(){
     body.style.background = 
         "linear-gradient(to right, "
@@ -34,12 +37,13 @@ function setGradient(){
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 6; i++) { // 6 is the length of the string letters
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
+//set the random background
 function setRandomGradient(){
     color1.value = getRandomColor();
     color2.value = getRandomColor();
@@ -55,17 +59,11 @@ function setRandomGradient(){
         + ", "
         +color2.value
         + ")";
-
         css.textContent = body.style.background + ";";
-        
-
 }
 
-
+//Events listeners to update the background
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
-
-
 btn.addEventListener("click", setRandomGradient);
 
